2fcf667f95c0f31c624c0fe0d7bcaf756882ba0d
Joyce Achieng <joyce.achieng@student.moringaschool.com>
2023-09-18 21:57:04 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random() * 100) + 1;
+
+let num3 = 40
+let num4 = 6
+let mod =num3 % num4;
+
+let values = (4,6,10,20);
+let max = Math.max(values);
